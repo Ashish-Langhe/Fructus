5dd56dd96c68bfc73adfc54165fed454fd4c60fb
Ashish-Langhe <langheashish389@gmail.com>
2021-12-04 15:20:18 +0530

    Added files

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by user208584 on 12/4/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
