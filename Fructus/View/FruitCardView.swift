305f14afccec6c1784cff88aa397f355186258fb
Ashish-Langhe <langheashish389@gmail.com>
2021-12-04 16:16:37 +0530

    Updated

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,28 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Ashish Langhe on 12/4/21.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    //MARK: - PROPERTIES
+    
+    
+    
+    //MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//MARK: - PREVIEW
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    
+    }
+}
